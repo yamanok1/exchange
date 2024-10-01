df5af87ea67a01112bd79ab75bbe7e679be1cf2c
Yaman Ok <ahmetyamanok@gmail.com>
2024-10-01 11:43:49 +0300

    exchange
    Exchange

---
+++ currency-conveter/src/App.jsx
@@ -0,0 +1,12 @@
+import ConverterForm from "./components/ConverterForm";
+
+const App = () => {
+  return (
+    <div className="currency-conventer">
+      <h2 className="conventer-title">Corrency Conventer</h2>
+      <ConverterForm />
+    </div>
+  );
+};
+
+export default App;
